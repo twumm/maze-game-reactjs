1ecd4645aa4f0f9598da366177c14f53d95cc68f
twumm <martint.mensah@gmail.com>
2018-07-23 22:26:53 +0000

    feat: Display squares/cells with sprites hidden.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,49 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import player from './anonymous_hacker.png'
+import sprite from './bitcoin.png'
+
+let squareArea = []
+let customBoard = []
 
 // Create a square
-const Square = (props) => {
+/*const Square = (props) => {
   return (
-    <button className="square">
-      {props.value}
+    <button
+      className="square"
+      onClick={() => alert(props.value)}
+    >
+    <img className="player-icon" src={player}>
+    </img>
     </button>
   )
+}*/
+
+class Square extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: null,
+    }
+  }
+
+  showIcon() {
+    this.setState({display: !this.state.display})
+  }
+  
+  render() {
+    return (
+      <button 
+        className="square"
+        /*onClick={() => !this.state.display}
+        style={{display: 'show'}}
+        // onKeyPress={() => alert(this.props.value)}*/
+      >
+        <img className="player-icon" src={player} />
+        <img className="sprite-icon" src={sprite} />
+      </button>
+    )
+  }
 }
 
 // Create a player
@@ -24,11 +59,13 @@ const Player = (props) => {
 class Board extends React.Component {
 
   createSquares() {
-    let customBoard = []
-    let customRow = prompt('Please enter number of rows', 2)
-    let customColumn = prompt('Please enter number of columns', 2)
+    // let customBoard = []
+    let customRow = 4
+    // let customRow = prompt('Please enter number of rows', 2)
+    let customColumn = 4
+    // let customColumn = prompt('Please enter number of columns', 2)
 
-    while (customRow < 2 || isNaN(customRow)) {
+    /*while (customRow < 2 || isNaN(customRow)) {
       customRow = prompt('Please enter number of rows.' + 
         'Only numbers greater than 1 are allowed.', 2)
     }
@@ -36,23 +73,33 @@ class Board extends React.Component {
     while (customColumn < 2 || isNaN(customColumn)) {
       customColumn = prompt('Please enter number of columns.' + 
         'Only numbers greater than 1 are allowed.', 2)
-    }
+    }*/
 
     for (let row = 0; row < customRow; row++) {
-      let boardSquareRow = []
+      let boardRow = []
       for (let column = 0; column < customColumn; column++) {
-        boardSquareRow.push(<Square />)
+        boardRow.push(<Square value={row.toString() + column.toString()} key={row.toString() + column.toString()}/>)
+        squareArea.push(`${row}${column}`)
       }
-      customBoard.push(<div className="board-row">{ boardSquareRow }</div>)
+      customBoard.push(<div className="board-row">{ boardRow }</div>)
     }
     return customBoard
+  }
+
+  handlePlayerPosition() {
+    // console.log(customBoard[2].props.children)
+    // squareArea[4]
+    /*squareArea[2] = this.setState({
+      value: <img src={player}/>
+    })*/
+
   }
 
   render() {
     return (
       <div>
         {this.createSquares()}
-        
+        {this.handlePlayerPosition()}
       </div>
     )
   }
@@ -63,11 +110,25 @@ class Sprite extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      player: <Player />
+    }
+  }
+
+  handlePlayer() {
+    const player = this.state.player
+    console.log('e')
+    /*if (event.keyCode === 38) {
+      console.log('Entered')
+    }*/
+  }
+
+  
   render() {
     return (
-      <div>
-        <Board />
-      </div>
+      <Board />
     )
   }
 }
