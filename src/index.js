cdb16641cb56949c89eab041fad2b502b18c3616
twumm <martint.mensah@gmail.com>
2018-07-23 10:20:49 +0000

    feat: Create a player variable with an icon.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import player from './anonymous_hacker.png'
 
 // Create a square
 const Square = (props) => {
@@ -8,6 +9,15 @@ const Square = (props) => {
     <button className="square">
       {props.value}
     </button>
+  )
+}
+
+// Create a player
+const Player = (props) => {
+  return (
+    <img className="player-icon" src={player}>
+      {props.value}
+    </img>
   )
 }
 
